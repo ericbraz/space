2b6567bf1af0e23fc2d3ee2dedafaac6879ed4c6
ericbraz <89194596+ericbraz@users.noreply.github.com>
2023-08-24 10:06:06 -0300

    second class

--- galery/views.py
+++ galery/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from galery.models import Fotografia
+
 
 def index(request):
-    return render(request, 'galery/index.html')
+    data = Fotografia.objects.order_by("-data_fotografia").filter(publicada=True)
+    return render(request, "galery/index.html", {"cards": data})
 
-def imagem(request):
-    return render(request, 'galery/imagem.html')
+
+def imagem(request, foto_id):
+    data = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, "galery/imagem.html", {"fotografia": data})
+
+def buscar(request):
+    data = Fotografia.objects.order_by("-data_fotografia").filter(publicada=True)
+
+    if "buscar" in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if nome_a_buscar:
+            data = data.filter(nome__contains=nome_a_buscar)
+
+    return render(request, 'galery/buscar.html', {"cards": data})
